0a7e49f3db16e48fcbb755ce348ee680d64eb256
Best Cat <kotova-irina11@rambler.ru>
2022-12-04 15:28:39 +0300

    лекция 2 file

---
+++ lection2_file/lec2_file.py
@@ -0,0 +1,23 @@
+file = open('ddd','a')
+
+
+
+
+
+#with open('file.txt', 'w') as data:
+# data.write('line 1\n')
+# data.write('line 2\n')
+
+
+
+#colors = ['red', 'green', 'blue']
+##data = open('file.txt', 'w')
+#data.writelines(colors) # разделителей не будет
+#data.close()
+
+
+#path = 'file.txt'
+#data = open(path, 'r')
+#for line in data:
+ #print(line)
+#data.close()
